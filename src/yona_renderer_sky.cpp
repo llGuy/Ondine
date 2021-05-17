63dc40a7f92888e495daff5843030f7015294611
llGuy <luc.rosenzweig@yahoo.com>
2021-05-17 22:58:38 +0100

    Added values for earth's atmosphere

--- src/yona_renderer_sky.cpp
+++ src/yona_renderer_sky.cpp
@@ -22,6 +22,47 @@ void RendererSky::initSkyProperties(VulkanContext &graphicsContext) {
     graphicsContext.descriptorPool(),
     graphicsContext.descriptorLayouts(),
     makeArray<VulkanBuffer, AllocationType::Linear>(mSkyPropertiesBuffer));
+
+  /* 
+     Based on the values of Eric Bruneton's atmosphere model.
+     We aren't going to calculate these manually come on (at least not yet)
+  */
+
+  /* 
+     These are the irradiance values at the top of the Earth's atmosphere
+     for the wavelengths 680nm (red), 550nm (green) and 440nm (blue) 
+     respectively. These are in W/m2
+  */
+  mSkyProperties.solarIrradiance = glm::vec3(1.474f, 1.8504f, 1.91198f);
+  // Angular radius of the Sun
+  mSkyProperties.solarAngularRadius = 0.004675f;
+  mSkyProperties.bottomRadius = 6360.0f;
+  mSkyProperties.topRadius = 6420.0f;
+
+  mSkyProperties.rayleighDensity.layers[0] =
+    DensityLayer { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+  mSkyProperties.rayleighDensity.layers[1] =
+    DensityLayer { 0.0f, 1.0f, -0.125f, 0.0f, 0.0f };
+  mSkyProperties.rayleighScatteringCoef =
+    glm::vec3(0.005802f, 0.013558f, 0.033100f);
+
+  mSkyProperties.mieDensity.layers[0] =
+    DensityLayer { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+  mSkyProperties.mieDensity.layers[1] =
+    DensityLayer { 0.0f, 1.0f, -0.833333f, 0.0f, 0.0f };
+  mSkyProperties.mieScatteringCoef = glm::vec3(0.003996f, 0.003996f, 0.003996f);
+  mSkyProperties.mieExtinctionCoef = glm::vec3(0.004440f, 0.004440f, 0.004440f);
+
+  mSkyProperties.miePhaseFunctionG = 0.8f;
+
+  mSkyProperties.absorptionDensity.layers[0] =
+    DensityLayer { 25.000000f, 0.000000f, 0.000000f, 0.066667f, -0.666667f };
+  mSkyProperties.absorptionDensity.layers[1] =
+    DensityLayer { 0.000000f, 0.000000f, 0.000000f, -0.066667f, 2.666667f };
+  mSkyProperties.absorptionExtinctionCoef =
+    glm::vec3(0.000650f, 0.001881f, 0.000085f);
+  mSkyProperties.groundAlbedo = glm::vec3(0.100000f, 0.100000f, 0.100000f);
+  mSkyProperties.muSunMin = -0.207912f;
 }
 
 void RendererSky::preparePrecompute(VulkanContext &graphicsContext) {
